8b4461227ba5ef48643638f1d34f59ce0ca01e64
Marius K <marius.into@gmail.com>
2023-01-16 16:26:01 +0200

    add react-google-charts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Chart } from "react-google-charts";
 
-function App() {
+const color = "color:rgb(51, 102, 204)"
+
+export const options = {
+  title: "Realaus laiko rezultatai",
+  //vAxis: { title: "Lapai", viewWindow: { min: 0, max: 16 } },
+  legend: "none"
+};
+
+export const data = [
+  ["Element", "Lapai", { role: "style" }],
+  ["Nestingas #1", 12, color],
+  ["Nestingas #2", 5, color], 
+  ["Nestingas #3", 10, color],
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart chartType="ColumnChart" width="800px" height="400px" data={data} options={options} />
   );
 }
-
-export default App;
