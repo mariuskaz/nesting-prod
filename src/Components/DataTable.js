3108aac0e7e5860bee35f6f2ca4ac51f17906251
Marius K <marius.into@gmail.com>
2023-01-25 17:13:00 +0200

    add save button

--- src/Components/DataTable.js
+++ src/Components/DataTable.js
@@ -11,7 +11,7 @@ export default function DataTable({ items }) {
   }
 
   const style= { 
-    margin:'15px 5px', 
+    margin:'5px 5px 50px',
   }
 
   const format = t => { 
@@ -24,8 +24,24 @@ export default function DataTable({ items }) {
         [ item.machine, format(item.start), format(item.end), item.duration, item.name, item.failed, item.type ]),
   ]
 
+  function handleSave() {
+    console.log('download file')
+    let content = "Nestingas;Startas;Pabaiga;Programa;Sutrikimai;Programos tipas\n"
+    let link = document.createElement('a')
+    items.forEach(item => content += `${item.machine};${item.start};${item.end};${item.name};${item.failed};${item.type}\n` )
+    let bom = new Uint8Array([0xEF, 0xBB, 0xBF]) // UTF-8 BOM
+    let blob = new Blob([bom, content], {type: 'text/html'})
+    link.style.display = 'none'
+    document.body.appendChild(link)
+    link.href = URL.createObjectURL(blob)
+    link.download = "results.csv"
+    link.click()
+    URL.revokeObjectURL(link.href)
+  }
+
   return (
     <>
+      <div className="material-symbols-sharp action-button" onClick={handleSave}>save</div>
       <Chart
         chartType="Table"
         data={data}
