02a0727d0323604b2375b7bfd2853f8456925fc8
Marius K <marius.into@gmail.com>
2023-02-12 12:57:09 +0200

    update params

--- src/Components/Params.js
+++ src/Components/Params.js
@@ -4,10 +4,11 @@ import CheckBox from "./CheckBox"
 export default function Params({ params, toggleIdle, toggleExpand }) {
   return (
     <div className="box">
-        <p>PARAMETRAI</p>
-        <CheckBox label={'Įtraukti staklių prastovas į rodiklius'} value={params.calcIdle} onChange={toggleIdle} />
-        <CheckBox label={'Rodyti išskleistus filtrus'} value={params.expandAll} onChange={toggleExpand} />
-        <br/>
+        <p style={{marginBottom:'20px'}}>PARAMETRAI</p>
+        <p>
+          <CheckBox label={'Įtraukti staklių prastovas į rodiklius'} value={params.calcIdle} onChange={toggleIdle} />
+          <CheckBox label={'Rodyti išskleistus filtrus'} value={params.expandAll} onChange={toggleExpand} />
+        </p>
       </div>
   )
 }
