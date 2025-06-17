a6df0069a3557ab3fb4d01dd0190320702b8b695
Marius K <marius.into@gmail.com>
2025-06-17 14:54:03 +0300

    fix params

--- src/Components/Params.js
+++ src/Components/Params.js
@@ -26,7 +26,7 @@ export default function Params({ params, toggleIdle, toggleExpand, items }) {
     <>
       <div className="box">
         <p className="bold">PARAMETRAI</p>
-        <CheckBox label={'Įtraukti staklių prastovas į rodiklius'} value={params.calcIdle} onChange={toggleIdle} />
+        <CheckBox label={'Įtraukti staklių prastovas į rodiklius'} value={params.calcIdleTime} onChange={toggleIdle} />
         <CheckBox label={'Rodyti išskleistus filtrus'} value={params.expandAll} onChange={toggleExpand} />
         <br/>
       </div>
