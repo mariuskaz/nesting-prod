1302da0f498c0e8d90df0da315c664a7c8cb919f
Marius K <marius.into@gmail.com>
2025-08-01 13:56:04 +0300

    convert to named function

--- src/Components/Stats.js
+++ src/Components/Stats.js
@@ -1,30 +1,36 @@
 import React, { useEffect } from 'react';
 
-const Title = ({name, desc}) => (
-    <div className="box">
-        <p className="bold">{name}<br/>
-        <small>{desc}</small></p>
-    </div>
-)
+function Title({ name, desc }) {
+    return (
+        <div className="box">
+            <p className="bold">{name}<br />
+                <small>{desc}</small></p>
+        </div>
+    );
+}
 
-const PowerStats = ({style, on, off}) => (
-    <div className="box" style={{ padding: "20px 10px" }}>
-        <div className='inline'>Power On <span style={style}>{on}</span></div>
-        <div className='inline'>Power Off <span style={style}>{off}</span></div>
-    </div>
-)   
+function PowerStats({ style, on, off }) {
+    return (
+        <div className="box" style={{ padding: "20px 10px" }}>
+            <div className='inline'>Power On <span style={style}>{on}</span></div>
+            <div className='inline'>Power Off <span style={style}>{off}</span></div>
+        </div>
+    );
+}   
 
-const WorkingStats = ({rows}) => (
-    <div className="box">
-        <table className="stat-table">
-            <tbody>{rows}</tbody>
-        </table>
-    </div>
-);
+function WorkingStats({ rows }) {
+    return (
+        <div className="box">
+            <table className="stat-table">
+                <tbody>{rows}</tbody>
+            </table>
+        </div>
+    );
+}
 
-export default function Stats({date, stats, loaded}) {
+export default function Stats({ date, stats, loaded }) {
     const [synced, setSynced] = React.useState(false);
-    const defaultTimes = "00:00:00";
+    const defaultTime = "00:00:00";
 
     useEffect(() => {
         if (loaded) setSynced(true);
@@ -36,15 +42,15 @@ export default function Stats({date, stats, loaded}) {
     const desc = "BIESSE AKRON 1330-A S/N 1000061614";
     
     const values = {
-        "Apdirbtos plokštės": stats.panels,
-        "Apdirbti tiesiniai metrai": stats.meters,  
-        "Programos paruošimo laikas": defaultTimes,
-        "Programos keitimų skaičius": stats.programs,
-        "Rankinio paruošimo laikas": defaultTimes,
-        "Staklių techninės priežiūros laikas": defaultTimes,
-        "Staklių avarinės būsenos laikas": defaultTimes,
-        "Staklių darbo laikas": stats.working,
-        "Tuščių staklių darbo laikas": defaultTimes,
+        "Apdirbtos plokštės": stats.panels || 0,
+        "Apdirbti tiesiniai metrai": stats.meters || 0,  
+        "Programos paruošimo laikas": stats.prepare || defaultTime,
+        "Programos keitimų skaičius": stats.programs || 0,
+        "Rankinio paruošimo laikas": stats.manual || defaultTime,
+        "Staklių techninės priežiūros laikas": stats.maintenance || defaultTime,
+        "Staklių avarinės būsenos laikas": stats.alerts || defaultTime,
+        "Staklių darbo laikas": stats.working || defaultTime,
+        "Tuščių staklių darbo laikas": stats.empty || defaultTime,
     };
 
     const style = {
